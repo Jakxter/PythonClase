ee7a09f1ea65efde600bbc94af78aeeca0fc113c
Jakxter <122190555+Jakxter@users.noreply.github.com>
2023-01-21 19:31:41 -0500

    Create Ejercicio3.py

---
+++ LISTA DE EJERCICIO NRO 2/Ejercicio3.py
@@ -0,0 +1,7 @@
+Valor1=int(input("Ingrese el primero valor: "))
+Valor2=int(input("Ingrese el segundo valor: "))
+
+if Valor1<Valor2:
+    print("El mayor valor es: ",Valor2)
+else:
+    print("El mayo valor es: ",Valor1)
