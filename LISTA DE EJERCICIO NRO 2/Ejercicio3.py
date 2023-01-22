09b99cb881f5cf37f8759159b8f023da38dea798
Jakxter <122190555+Jakxter@users.noreply.github.com>
2023-01-21 22:59:20 -0500

    Update Ejercicio3.py

--- LISTA DE EJERCICIO NRO 2/Ejercicio3.py
+++ LISTA DE EJERCICIO NRO 2/Ejercicio3.py
@@ -1,7 +1,11 @@
 Valor1=int(input("Ingrese el primero valor: "))
 Valor2=int(input("Ingrese el segundo valor: "))
 
-if Valor1<Valor2:
-    print("El mayor valor es: ",Valor2)
-else:
-    print("El mayo valor es: ",Valor1)
+def Numero(Valor1, Valor2):
+    
+    if Valor1 > Valor2:
+        return Valor1
+    else:
+        return Valor2
+
+print("El número mayor es: ", Numero(Valor1, Valor2))
